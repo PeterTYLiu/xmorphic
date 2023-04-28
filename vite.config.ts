daff795af7f096f014c2bb4982a756fa8ca330d7
PeterTYLiu <Peter.ty.liu@gmail.com>
2023-04-28 18:49:17 -0400

    add icons for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,31 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "icon-192.png", "icon-512.png"],
+
+      manifest: {
+        name: "𝑥morphic",
+        short_name: "𝑥morphic",
+        description: "Physics-based realistic CSS generator",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   base: "/",
 });
